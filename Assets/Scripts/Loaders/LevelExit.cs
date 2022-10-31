91e999e8786ab6bb056c2ca5491df86efdd5dabb
JackMulford <JM257511@falmouth.ac.uk>
2022-10-31 14:50:45 +0000

    25th Commit

---
+++ Assets/Scripts/Loaders/LevelExit.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+
+    [SerializeField] float LevelLoadDelay = 2f;
+    [SerializeField] float LevelExitSlowMoFactor = 0.2f;
+    [SerializeField] AudioClip exitSound;
+    [SerializeField][Range(0, 1)] float exitSoundVolume = 0.25f;
+
+    void OnTriggerEnter2D(Collider2D Player)
+    {
+        StartCoroutine(LoadNextLevel());
+        AudioSource.PlayClipAtPoint(exitSound, Camera.main.transform.position, exitSoundVolume);
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = LevelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        Time.timeScale = 1f;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+}
---
+++ Assets/Scripts/Loaders/SceneLoader.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadPreviousScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex - 1);
+    }
+
+    public void LoadControlsScreen()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void LoadPreMenu()
+    {
+        SceneManager.LoadScene("PreMenu");
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+
+}
